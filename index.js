3a8f09b2d84ce1d6fefeb19bdc9565140b7471a6
sabyasachirakshit <sabyasachirakshitacs19@gmail.com>
2023-08-28 20:55:29 +0530

    exporting module and importing

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-require("./add.js");
-require("./hello.js");
+const importedAddFunction = require("./add.js");
+
+const sum = importedAddFunction(5, 5);
+const sumcopy = importedAddFunction(5, 10);
+console.log(sum);
+console.log(sumcopy);
 console.log("Index file");
